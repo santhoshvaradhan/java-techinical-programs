2af010efb741d5d24672ce9fa024885bf7470f3f
SANTHOSH V <115791144+santhoshvaradhan@users.noreply.github.com>
2022-12-09 14:32:48 +0530

    Create intermediate array rotation.java
    This program  say about rotation process elements in the array

---
+++ intermediate array rotation.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Rotate an Array in Java");
+        int i,j,k,n;
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter the number of elements:");
+        n=in.nextInt();
+        int [] nums=new int[n];
+        int [] result=new int[n];
+        System.out.println("Enter the elements:");
+        for(i=0;i<nums.length;i++)
+        {
+            nums[i]=in.nextInt();
+        }
+        System.out.println("Enter number of step to rotate:");
+        //k is step value which means number of step to rotate;
+        k=in.nextInt();
+        // for loop for rotate the elements ;
+        for(i=0;i<k;i++)
+        {
+            result[i]=nums[nums.length-k+i];
+        }
+        for(j=i;j<nums.length;j++)
+        {
+            result[j]=nums[j-i];
+        }
+        System.out.println("Array after rotation:");
+        for(i=0;i<nums.length;i++)
+        {
+            System.out.println(result[i]);
+        }
+    }
+}
